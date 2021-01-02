0ac29abdcb74efb7fa4069d4d3b8f456294fb6ec
Pascal Berger <pascal.berger@gmail.com>
2021-01-02 21:12:06 +0100

    Add Cake.Frosting 1.0 RC2 example

---
+++ build/Program.cs
@@ -0,0 +1,72 @@
+using System.Threading.Tasks;
+using Cake.Common;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNetCore;
+using Cake.Common.Tools.DotNetCore.Build;
+using Cake.Common.Tools.DotNetCore.Test;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Frosting;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        return new CakeHost()
+            .UseContext<BuildContext>()
+            .UseWorkingDirectory("..")
+            .Run(args);
+    }
+}
+
+public class BuildContext : FrostingContext
+{
+    public string MsBuildConfiguration { get; set; }
+
+    public BuildContext(ICakeContext context)
+        : base(context)
+    {
+        MsBuildConfiguration = context.Argument("configuration", "Release");
+    }}
+
+[TaskName("Clean")]
+public sealed class CleanTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.CleanDirectory($"./src/Example/bin/{context.MsBuildConfiguration}");
+    }
+}
+
+[TaskName("Build")]
+[IsDependentOn(typeof(CleanTask))]
+public sealed class BuildTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetCoreBuild("./src/Example.sln", new DotNetCoreBuildSettings
+        {
+            Configuration = context.MsBuildConfiguration,
+        });
+    }
+}
+
+[TaskName("Test")]
+[IsDependentOn(typeof(BuildTask))]
+public sealed class TestTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetCoreTest("./src/Example.sln", new DotNetCoreTestSettings
+        {
+            Configuration = context.MsBuildConfiguration,
+            NoBuild = true,
+        });
+    }
+}
+
+[TaskName("Default")]
+[IsDependentOn(typeof(TestTask))]
+public sealed class Default : FrostingTask
+{
+}
